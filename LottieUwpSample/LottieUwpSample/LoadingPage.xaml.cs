7796b6629283591e3e3dfabfb0635e9c72f49aec
Kirk Patrick Junsay <kirk.patrick.junsay@osm.no>
2019-07-25 14:08:32 +0800

    Add project files.

---
+++ LottieUwpSample/LottieUwpSample/LoadingPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LottieUwpSample
+{
+	public partial class LoadingPage : PopupPage
+	{
+        public LoadingPage()
+        {
+            InitializeComponent();
+        }
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            await PopupNavigation.Instance.PopAllAsync();
+        }
+    }
+}
---
+++ LottieUwpSample/LottieUwpSample/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+using Xamarin.Forms;
+
+namespace LottieUwpSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            await PopupNavigation.Instance.PushAsync(new LoadingPage());
+        }
+    }
+}
